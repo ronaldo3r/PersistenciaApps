c6106e8324171e66ed91ed9f768b3edcd2538b84
ronaldo <ronaldo3-0827@hotmail.com>
2019-03-14 20:14:12 -0500

    Crear tabla amigos

--- app/src/main/java/appmoviles/com/appsmoviles20191/db/DBHandler.java
+++ app/src/main/java/appmoviles/com/appsmoviles20191/db/DBHandler.java
@@ -4,18 +4,27 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
-public class DBHandler extends SQLiteOpenHelper{
+public class DBHandler extends SQLiteOpenHelper {
 
     public static final String DB_NAME = "AppMoviles20191";
     public static final int DB_VERSION = 1;
+
+    //TABLA AMIGOS
+    public static final String TABLE_AMIGOS = "amigos";
+    public static final String AMIGOS_ID = "id";
+    public static final String AMIGOS_NOMBRE = "nombre";
+    public static final String AMIGOS_EDAD = "edad";
+    public static final String AMIGOS_CORREO = "correo";
+    public static final String AMIGOS_TELEFONO = "telefono";
+    public static final String CREATE_TABLE_AMIGOS = "CREATE TABLE "+TABLE_AMIGOS+" ("+AMIGOS_ID+" TEXT PRIMARY KEY, "+AMIGOS_NOMBRE+" TEXT, "+AMIGOS_EDAD+" TEXT, "+AMIGOS_CORREO+" TEXT, "+AMIGOS_TELEFONO+" TEXT)";
 
     public DBHandler(Context context) {
         super(context, DB_NAME, null, DB_VERSION);
     }
 
     @Override
-    public void onCreate(SQLiteDatabase sqLiteDatabase) {
-
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE_AMIGOS);
     }
 
     @Override
